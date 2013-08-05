4ca6ca9b9e91486a47c2cc054ddcf6b4095ff2a8
Stefan Grassberger <stefan.grassberger@gmail.com>
2013-08-05 11:33:59 +0200

    Basic controls, skybox, sun

---
+++ Assets/MouseControls.cs
@@ -0,0 +1,36 @@
+﻿// Credit to damien_oconnell from http://forum.unity3d.com/threads/39513-Click-drag-camera-movement
+// for using the mouse displacement for calculating the amount of camera movement and panning code.
+
+using UnityEngine;
+using System.Collections;
+
+public class MouseControls : MonoBehaviour {
+  
+	public float turnSpeed;		// Speed of camera turning when mouse moves in along an axis
+	
+	private Vector3 dragOrigin;	// Position of cursor when drag starts
+	private bool isRotating;	// Is the camera being rotated?
+	
+	void Update ()
+	{
+		// Get the left mouse button
+		if(Input.GetMouseButtonDown(0))
+		{
+			// Get drag origin
+			dragOrigin = Input.mousePosition;
+			isRotating = true;
+		}
+		
+		// Disable movements on button release
+		if (!Input.GetMouseButton(0)) isRotating = false;
+		
+		// Rotate camera along X and Y axis
+		if (isRotating)
+		{
+	        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+
+			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+		}
+	}
+}
---
+++ Assets/ShipControls.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+public var moveSpeed : float = 10f;
+public var strafeSpeed : float = 5f;
+public var turnSpeed : float = 50f;
+
+function Start () {
+
+}
+
+function Update ()
+{
+	// Speed
+    if(Input.GetKey(KeyCode.W))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.S))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+    
+    // Strafe
+    if(Input.GetKey(KeyCode.A))
+        transform.Translate(Vector3.left * strafeSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.D))
+        transform.Translate(Vector3.right * strafeSpeed * Time.deltaTime);
+    
+    // Turn
+    /*if(Input.GetKey(KeyCode.Q))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.E))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        
+	if(Input.GetKey(KeyCode.W))
+        transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+    
+    if(Input.GetKey(KeyCode.S))
+        transform.Rotate(Vector3.left, turnSpeed * Time.deltaTime);*/
+}
